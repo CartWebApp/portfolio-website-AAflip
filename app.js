7dc878f01009bd5cc753520181e19f8a17d19c24
AAflip <jedidjm2008@gmail.com>
2025-05-12 15:49:08 -0700

    kill me please

---
+++ app.js
@@ -0,0 +1,6 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     document.getElementsByClassName('portCheck').addEventListener('click', () => {
+//         // document.getElementById('rainiestCard').style.height = '300px';
+//         console.log('h')
+//     });
+// });
