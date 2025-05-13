975b8a6f7789e5f8c62a1d63b5b7ef5bc61d1ca5
AAflip <jedidjm2008@gmail.com>
2025-05-13 15:47:49 -0700

    somestuff

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     document.getElementsByClassName('portCheck').addEventListener('click', () => {
-//         // document.getElementById('rainiestCard').style.height = '300px';
-//         console.log('h')
-//     });
-// });
+document.addEventListener('DOMContentLoaded', () => {
+    let checkBox = document.getElementById('menuCheck');
+    let navBar = document.getElementById('navigation');
+    let closeBox = document.getElementById('menuCheckOff');
+
+    checkBox.addEventListener('change', () => {
+        if(checkBox.checked){
+            navBar.style.width = '500px';
+        }else{
+            navBar.style.width = '0';
+        }
+    });
+
+    closeBox.addEventListener('change', () => {
+        if(checkBox.checked){
+            checkBox.checked = false;
+            closeBox.checked = false;
+            navBar.style.width = '0';
+        }
+    });
+});
